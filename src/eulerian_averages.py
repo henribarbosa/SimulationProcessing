f7620b1f37e24602e54dbd78c04a9a80942f574d
user <henribarbosa.o@gmail.com>
2024-01-12 15:17:00 -0300

    sex jan 12 15:17:00 -03 2024

--- src/eulerian_averages.py
+++ src/eulerian_averages.py
@@ -1,5 +1,6 @@
 import numpy as np
 from bed_points_array import points_array
+from read_files import time_step_data, simulation_data
 
 # Average the desired measument in the points of the bed
 class eulerian_average:
